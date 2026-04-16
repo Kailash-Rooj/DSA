7b4e2e0017c4ee42b40a754d8f3be88c2ca444f0
Kailash-Rooj <kailashrooj33@gmail.com>
2026-04-17 00:34:14 +0530

    Reverse Number

---
+++ Basic_math/p01.java
@@ -0,0 +1,26 @@
+// Problem Statement: Given an integer N return the reverse of the given number.
+
+import java.util.Scanner;
+
+
+public class p01 {
+    public static void main(String[] args){
+        int n , count = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter no: ");
+        n = sc.nextInt();
+        System.out.println("After reversing: " + fn(n));
+    }
+
+    public static int fn(int n){
+        int flag = (n < 0) ? 1 : 0; // 1 = -ve
+        n = Math.abs(n);
+        int revNo = 0;
+        while(n > 0){
+            revNo = (n % 10) + (revNo * 10);
+            n /= 10;
+        }
+        return (flag == 1)?-revNo:revNo;
+    }
+
+}
