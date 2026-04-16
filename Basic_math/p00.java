1a42cf7ae471136383cec3746c184374e9b54962
Kailash-Rooj <kailashrooj33@gmail.com>
2026-04-17 00:06:26 +0530

    Count Digit

---
+++ Basic_math/p00.java
@@ -0,0 +1,35 @@
+// Problem Statement: Given an integer N, print the number of digits in N.
+import java.util.Scanner;
+
+
+public class p00 {
+    public static void main(String[] args){
+        int n , count = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter no: ");
+        n = sc.nextInt();
+        System.out.println("Number of digits = " + fn1(n));
+        System.out.println("Number of digits = " + fn2(n));
+    }
+
+    // Time: O(log10 N)  |  Space: O(1)
+    public static int fn1(int n){
+        int count = 0;
+        if(n == 0) return 1;
+        n = Math.abs(n);
+
+        while (n > 0) {
+            count++;
+            n /= 10;
+        }
+        return count;
+    }
+
+    //Mathematical approach (log10) 
+    // Time: O(1)  |  Space: O(1)
+    public static int fn2(int n){
+        if(n == 0)return 1;
+        n = Math.abs(n);
+        return 1 + (int)Math.log10(n);
+    }
+}
