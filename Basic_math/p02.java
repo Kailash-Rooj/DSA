cf317ec1b5aa1e7116e24d4529dbe97c7bea49c8
Kailash-Rooj <kailashrooj33@gmail.com>
2026-04-17 00:59:09 +0530

    Palindrome number

---
+++ Basic_math/p02.java
@@ -0,0 +1,30 @@
+/*Problem Statement: Given an integer N, return true 
+if it is a palindrome else return false.*/
+
+import java.util.Scanner;
+
+public class p02 {
+    public static void main(String[] args){
+        int n , count = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter no: ");
+        n = sc.nextInt();
+        
+        if(fn(n)){
+            System.out.println("Palindrome");
+        }else{
+            System.out.println("Not Palindrome");   
+        }
+    }
+
+    public static boolean fn(int n){
+        if((n < 0) || (n % 10 == 0 && n != 0))return false;
+        int revNo = 0;
+        while(n > revNo){
+            revNo = (n % 10) + (revNo * 10);
+            n /= 10;
+        }
+        return (revNo == n) || (revNo/10 == n);
+    }
+
+}
